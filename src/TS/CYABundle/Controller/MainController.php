c5c801dc67909f7dfbc4530b8c81e1ffd467158b
Roberto SLopez <roberto@tscompany.org>
2016-02-08 16:57:04 -0600

    add mail function

--- src/TS/CYABundle/Controller/MainController.php
+++ src/TS/CYABundle/Controller/MainController.php
@@ -3,25 +3,32 @@
 namespace TS\CYABundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
-    Sensio\Bundle\FrameworkExtraBundle\Configuration\Security,
-    Sensio\Bundle\FrameworkExtraBundle\Configuration\Template,
-    Sensio\Bundle\FrameworkExtraBundle\Configuration\Method,
-    Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+Sensio\Bundle\FrameworkExtraBundle\Configuration\Security,
+Sensio\Bundle\FrameworkExtraBundle\Configuration\Template,
+Sensio\Bundle\FrameworkExtraBundle\Configuration\Method,
+Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
- * @Route("admin")
- * @Security("has_role('ROLE_ADMIN')")
- */
+* @Route("admin")
+* @Security("has_role('ROLE_ADMIN')")
+*/
 class MainController extends BaseController
 {
     /**
-     * @Route("/", name="main")
-     * @Template()
-     */
+    * @Route("/", name="main")
+    * @Template()
+    */
     public function indexAction()
     {
-        return [
+        $message = \Swift_Message::newInstance()
+        ->setSubject('Registration confirmation')
+        ->setFrom('eldinsanchez@gmail.com')
+        ->setTo('tscompany09@gmail.com')
+        ->setContentType('text/plain')
+        ->setBody("test");
 
+        $this->get('mailer')->send($message);
+        return [
         ];
     }
 }
